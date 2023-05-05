4912af62ad53ce0fd2cb18379816d0a029671559
Саха <lopatkoaleksandr2@gmail.com>
2023-05-05 16:42:54 +0300

    feat: task 02-write-file done

---
+++ 02-write-file/index.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+
+const pathFile = path.join(__dirname, `${process.argv[2] ?? 'text'}.txt`);
+fs.access(pathFile, (err) =>{
+    if (err !== null) {
+        fs.writeFile(pathFile, '', (err) => {
+            // console.log(err);
+        })
+    }
+})
+process.stdin.on('data', (data) => {
+    fs.appendFile(pathFile, data.toString(), (err) => {
+        // console.log(err);
+    })
+})
+process.on('SIGINT', () => {
+    console.log('Bye!');
+    process.exit(0)
+})
+console.log('Hi, type some to console');
