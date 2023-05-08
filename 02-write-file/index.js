cec4691e00dbd2723d0147a8e9bbd09f9323e7e4
Саха <lopatkoaleksandr2@gmail.com>
2023-05-09 00:17:17 +0300

    fix: task 02 exit comment

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -10,12 +10,16 @@ fs.access(pathFile, (err) =>{
     }
 })
 process.stdin.on('data', (data) => {
+    if(data.toString() === 'exit\r\n') {
+        console.log('Bye!');
+        process.exit(0);
+    }
     fs.appendFile(pathFile, data.toString(), (err) => {
         // console.log(err);
     })
 })
 process.on('SIGINT', () => {
     console.log('Bye!');
-    process.exit(0)
+    process.exit(0);
 })
 console.log('Hi, type some to console');
